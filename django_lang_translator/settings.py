2af9ba64dfe0a5b129601592ff65acc2a963e306
WasekSamin <wasek.samin47@gmail.com>
2023-05-01 17:43:17 +0600

    1st may 2023

--- django_lang_translator/settings.py
+++ django_lang_translator/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-70wjpj+iwc+0xb4jqc9mlb^^@oo^vd)m$8$^nyd&03@rsa%sn)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
